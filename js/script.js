6c24f92adff50fd1e1f6151018a2455bf8264cc0
StacyNov <novikova.a0310@gmail.com>
2019-08-13 00:12:32 +0300

    burger+animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+$("#navToggle").click(function() {
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("open");
+    // this line ▼ prevents content scroll-behind
+    $("body").toggleClass("locked");
+});
 $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
